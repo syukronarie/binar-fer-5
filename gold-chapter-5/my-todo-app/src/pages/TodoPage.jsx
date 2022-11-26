d808dc8f55a95155391248843ad6e5ceda637675
syukronarie <syukronarie@gmail.com>
2022-11-26 22:03:17 +0700

    updated todo app

--- gold-chapter-5/my-todo-app/src/pages/TodoPage.jsx
+++ gold-chapter-5/my-todo-app/src/pages/TodoPage.jsx
@@ -4,6 +4,8 @@ const INITIAL_TODOS = [{ id: 1, text: "Learning js", isCompleted: false }];
 
 const TodoPage = () => {
 	const [todos, setTodos] = useState(INITIAL_TODOS);
+	const [showModal, setShowModal] = useState(false);
+	const [tempUpdateTodo, setTempUpdateTodo] = useState(null);
 
 	const handleOnSubmit = (e) => {
 		e.preventDefault();
@@ -40,32 +42,81 @@ const TodoPage = () => {
 		setTodos(newTodos);
 	};
 
-	return (
-		<div className="container">
-			<h1>My Todo App</h1>
-			<form onSubmit={handleOnSubmit}>
-				<input type="text" name="todo" id="todo" className="input" />
-				<button className="button" type="submit">
-					Add Todo
-				</button>
-			</form>
+	const handleShowModal = () => {
+		setShowModal(!showModal);
+	};
 
-			<div className="todos">
-				{todos.map((todo) => (
-					<div className="todos-item" key={todo.id}>
-						<p
-							onClick={() => handleCompleted(todo.id)}
-							className={`${
-								todo.isCompleted ? "todo-completed" : "todo-incompleted"
-							}`}
-						>
-							{todo.text}
-						</p>
-						<button onClick={() => handleDeleteTodo(todo.id)}>X</button>
-					</div>
-				))}
+	const handleChangeTodo = (e) => {
+		const text = e.target.value;
+		const { id, isCompleted } = tempUpdateTodo;
+		setTempUpdateTodo({ id, text, isCompleted });
+		const updatedTodo = { id, text, isCompleted };
+		setTodos((todos) => {
+			return todos.map((todo) => {
+				if (todo.id === id) {
+					todo = updatedTodo;
+				}
+				return todo;
+			});
+		});
+	};
+
+	const handleUpdateTodo = (todo) => {
+		setTempUpdateTodo(todo);
+		handleShowModal();
+	};
+
+	return (
+		<>
+			<div className="container">
+				<h1>My Todo App</h1>
+				<form onSubmit={handleOnSubmit}>
+					<input type="text" name="todo" id="todo" className="input" />
+					<button className="button" type="submit">
+						Add Todo
+					</button>
+				</form>
+
+				<div className="todos">
+					{todos.map((todo) => (
+						<div className="todos-item" key={todo.id}>
+							<p
+								onClick={() => handleCompleted(todo.id)}
+								className={`${
+									todo.isCompleted ? "todo-completed" : "todo-incompleted"
+								}`}
+							>
+								{todo.text}
+							</p>
+							<div>
+								<button onClick={() => handleDeleteTodo(todo.id)}>
+									␡ Delete
+								</button>
+								<button onClick={() => handleUpdateTodo(todo)}>✍️ Edit</button>
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
-		</div>
+			{showModal && (
+				<form className="modal" onSubmit={handleUpdateTodo}>
+					<div className="modal-update">
+						<input
+							type="text"
+							name="todo"
+							id="todo"
+							className="input"
+							value={tempUpdateTodo.text}
+							onChange={handleChangeTodo}
+						/>
+						<button className="button" type="submit">
+							Update Todo
+						</button>
+						<button onClick={handleShowModal}>Cancel</button>
+					</div>
+				</form>
+			)}
+		</>
 	);
 };
 
